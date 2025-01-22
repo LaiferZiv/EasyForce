5beb1e3fd2d3db3397f2464942b9cc5f90b20e22
laiferziv <laiferziv@gmail.com>
2025-01-22 19:55:50 +0200

    Phase 1 of initial data collection has been completed. In the next phase (Phase 2), we will proceed with data analysis. After Phase 2 of data analysis, we will add the capability to update and delete records in the database.

--- EasyForce/interface/user_questions_management/update_questions/update_entities.py
+++ EasyForce/interface/user_questions_management/update_questions/update_entities.py
@@ -1,3 +1,7 @@
+# from EasyForce.data_mangement.data_structure.entities_classes import Team, Soldier
+# from EasyForce.interface.user_questions_management.general_questions import ask_closed_ended_question, \
+#     ask_open_ended_question
+
 
 def update_TimeRange_questions():
     return
@@ -6,6 +10,17 @@ def update_Team_questions():
     return
 
 def update_Soldier_questions():
+    # teams_list = [t.TeamName for t in Team.get_all()]
+    # team_name = ask_closed_ended_question("Select the team the soldier is on:",teams_list)
+    # team_id = Team.get_by_unique_name(team_name)
+    # soldier_list = [f"{s.TeamName}, ID: {s.SoldierID}" for s in Soldier.get_all_by_column_value("TeamID",team_id)]
+    # chosen_soldier = ask_closed_ended_question("Select the soldier:",soldier_list)
+    # soldier_id = int(chosen_soldier.split("ID: ")[1])
+    # soldier = Soldier.get_by_id({"SoldierID":soldier_id})
+    # print(f"The current data: {soldier}")
+    # attributes = Soldier.get_columns()
+    # chosen_attribute = ask_closed_ended_question("Select the attributes you want to update",attributes)
+
     return
 
 def update_Role_questions():
